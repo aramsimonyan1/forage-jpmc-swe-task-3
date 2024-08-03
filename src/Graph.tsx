ec532c719b6bbfca93be3ac4af202ab7b319ea3d
aramsimonyan1 <aramsimonyan1@gmail.com>
2024-08-03 18:15:37 +0100

    DataManipulator.ts, Graph.tsx, .gitignore updated

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Table } from '@finos/perspective';
+import { Table, TableData } from '@finos/perspective';
 import { ServerRespond } from './DataStreamer';
 import { DataManipulator } from './DataManipulator';
 import './Graph.css';
@@ -23,10 +23,13 @@ class Graph extends Component<IProps, {}> {
     const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
     const schema = {
-      stock: 'string',
-      top_ask_price: 'float',
-      top_bid_price: 'float',
+      price_abc: 'float',
+      price_def: 'float',
+      ratio: 'float',
       timestamp: 'date',
+      upper_bound: 'float',
+      lower_bound: 'float',
+      trigger_alert: 'float',
     };
 
     if (window.perspective && window.perspective.worker()) {
@@ -35,24 +38,27 @@ class Graph extends Component<IProps, {}> {
     if (this.table) {
       // Load the `table` in the `<perspective-viewer>` DOM reference.
       elem.load(this.table);
-      elem.setAttribute('view', 'y_line');
-      elem.setAttribute('column-pivots', '["stock"]');
-      elem.setAttribute('row-pivots', '["timestamp"]');
-      elem.setAttribute('columns', '["top_ask_price"]');
+      elem.setAttribute('view', 'y_line');  //‘view’ is the kind of graph we want to visualize the data with. Since we want a continuous line graph we’re using a y_line
+      elem.setAttribute('row-pivots', '["timestamp"]'); // ‘row-pivots’ takes care of our x-axis. This allows us to map each datapoint based on its timestamp. Without this, the x-axis would be blank.
+      elem.setAttribute('columns', '["ratio", "lower_bound", "upper_bound", "trigger_alert"]'); // ‘columns’ is what allows us to focus on a particular part of a datapoint’s data along the y-axis. Without this, the graph would plot all the fields and values of each datapoint, yielding significant noise. We want to track ratio, lower_bound, upper_bound and trigger_alert.
+      // ‘aggregates’ allows us to handle the duplicate data and consolidate them into one data point. We only want to consider a data point unique if it has a timestamp. Otherwise, we’ll average the values of the other non-unique fields these ‘similar’ data points share before treating them as one (e.g. ratio, price_abc, …)
       elem.setAttribute('aggregates', JSON.stringify({
-        stock: 'distinctcount',
-        top_ask_price: 'avg',
-        top_bid_price: 'avg',
+        price_abc: 'avg',
+        price_def: 'avg',
+        ratio: 'avg',
         timestamp: 'distinct count',
+        upper_bound: 'avg',
+        lower_bound: 'avg',
+        trigger_alert: 'avg',
       }));
     }
   }
 
   componentDidUpdate() {
     if (this.table) {
-      this.table.update(
+      this.table.update([
         DataManipulator.generateRow(this.props.data),
-      );
+      ] as unknown as TableData);
     }
   }
 }
